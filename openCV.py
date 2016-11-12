7c1f46deee6ad4d43b7266bcbfddbead57075d19
Pawel Szudrowicz <pablo.szudrowicz@gmail.com>
2016-11-12 21:55:32 +0100

    Add video and haar xml classifier

--- openCV.py
+++ openCV.py
@@ -11,55 +11,67 @@ def calculateMedian(table):
     return sum // amount
 
 
-cap = cv2.VideoCapture('Motorway.mp4')
+cap = cv2.VideoCapture('video.mov')
 
 fgbg = cv2.createBackgroundSubtractorMOG2()
 fgbg2 = cv2.createBackgroundSubtractorKNN()
-
+#carCascade = cv2.CascadeClassifier('cars.xml')
 
 params = cv2.SimpleBlobDetector_Params()
 params.blobColor = 255
 params.filterByColor = True
-
+counter = 0
 detector = cv2.SimpleBlobDetector_create(parameters=params)
 
 while True:
     _, frame = cap.read()
-    #
     media = cv2.medianBlur(frame, 3)
-    blur = cv2.GaussianBlur(frame, (9,9),3)
+    blur = cv2.GaussianBlur(frame, (5,5),3)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    fgmask2 = fgbg2.apply(blur)
-    sobel = cv2.Canny(fgmask2, 150, 150)
+    fgmask2 = fgbg2.apply(media)
 
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (8, 8))
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
 
     close = cv2.morphologyEx(fgmask2, cv2.MORPH_CLOSE, kernel)
     opening = cv2.morphologyEx(close, cv2.MORPH_OPEN, kernel)
-    #erode = cv2.erode(close, (10,10))
-    #erode = cv2.erode(erode, (10, 10))
-    #erode = cv2.erode(erode, (10, 10))
-    dilation = cv2.dilate(opening, kernel, iterations=2)
-    thresh = 100
-    binary = (dilation > thresh) * 255
+    #erode = cv2.erode(opening, (10,10))
+    #dilation = cv2.dilate(opening, kernel, iterations=3)
+    thresh = 200
+    binary = (opening > thresh) * 255
     image = np.uint8(binary)
 
-    #keypoints = detector.detect(image)
-    #im_with_keypoints = cv2.drawKeypoints(dilaton, keypoints, np.array([]), (0, 0, 255))
+    keypoints = detector.detect(image)
 
-    # treshold = 5
-    # for x in range(0, dilaton.shape[0] // treshold):
-    #     for y in range(0, dilaton.shape[1] // treshold):
-    #         median = calculateMedian(dilaton[treshold * x:(treshold * x) + treshold, treshold * y:(treshold * y) + treshold])
+    for keypoint in keypoints:
+        _, y = keypoint.pt
+        if y > 380 and keypoint.class_id < counter:
+            counter = counter + 1
+            keypoint.class_id = counter
+            print('detected {}'.format(counter))
+
+
+    im_with_keypoints = cv2.drawKeypoints(image, keypoints, np.array([]), (0, 0, 255))
+
+    # treshold = 10
+    # for x in range(0, image.shape[0] // treshold):
+    #     for y in range(0, image.shape[1] // treshold):
+    #         median = calculateMedian(image[treshold * x:(treshold * x) + treshold, treshold * y:(treshold * y) + treshold])
     #
     #
-    #         if median > 100:
-    #             dilaton[treshold * x:(treshold * x) + treshold, treshold * y:(treshold * y) + treshold] = 255
+    #         if median > 250:
+    #             image[treshold * x:(treshold * x) + treshold, treshold * y:(treshold * y) + treshold] = 255
     #         else:
-    #             dilaton[treshold * x:(treshold * x) + treshold, treshold * y:(treshold * y) + treshold] = 0
+    #             image[treshold * x:(treshold * x) + treshold, treshold * y:(treshold * y) + treshold] = 0
 
-    cv2.imshow('fg',image)
+    # cars = carCascade.detectMultiScale(gray, 1.05, 5)
+    # for (x,y,w,h) in cars:
+    #     cv2.rectangle(frame, (x,y), (x+w, y+(h)), (255,0,0), 2)
+
+
+
+    cv2.line(im_with_keypoints, (0,380), (500,380), (255,0,0))
+    cv2.imshow('fg',im_with_keypoints)
     cv2.imshow('org',frame)
 
     k = cv2.waitKey(30) & 0xff
